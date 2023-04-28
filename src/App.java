2b523188ea108ebd9d6dce5620ebd4d28f070f62
Gustavo Auad Piccoli <gustavo.a.piccoli@gmail.com>
2023-04-28 15:52:00 -0300

    vai

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner; 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String texto = scanner.nextLine();
+            System.out.println("Você digitou: " + texto);
+            if (texto.compareTo("exit") == 0){
+                break;
+            }
+        }
+        scanner.close();
     }
 }
